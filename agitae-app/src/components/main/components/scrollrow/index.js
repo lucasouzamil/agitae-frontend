50951ddc5f60df721dba43b75c3b5b5e71da0000
Lucas Lima <lucasfsl@al.insper.edu.br>
2023-10-26 18:48:43 -0300

    comecando rolagem do carrossel

--- agitae-app/src/components/main/components/scrollrow/index.js
+++ agitae-app/src/components/main/components/scrollrow/index.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './scrollrow.css'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function ScrollRow(props) {
+    const [scrollX, setScrollX] = useState(0);
+
+    const handleLeftArrow = () => {
+        
+    };
+
+    const handleRightArrow = () => {
+       
+    };
+
     return (
         <div className="scrollRow">
-            <div className="scrollRow-left">
-                <KeyboardArrowLeftIcon style={{ fontSize: 50 }}></KeyboardArrowLeftIcon>
+            <div className="scrollRow-left" onClick={handleLeftArrow}>
+                <KeyboardArrowLeftIcon className="iconeScrollLeft" style={{ fontSize: 50 }}></KeyboardArrowLeftIcon>
             </div>
             <div className="scrollRow--listarea">
-                <div className="scrollRow--list">
+                <div className="scrollRow--list" style={{marginLeft:scrollX}}>
                     <div className="item--list">
                         <div className="item"><div className="teste"></div></div>
                         <div className="item"><div className="teste"></div></div>
@@ -21,8 +31,8 @@ export default function ScrollRow(props) {
                     </div>
                 </div>
             </div>
-            <div className="scrollRow-left">
-                <KeyboardArrowRightIcon style={{ fontSize: 50 }}></KeyboardArrowRightIcon>
+            <div className="scrollRow-left" onClick={handleRightArrow}>
+                <KeyboardArrowRightIcon className="iconeScrollRight" style={{ fontSize: 50 }}></KeyboardArrowRightIcon>
             </div>
         </div>
     );
