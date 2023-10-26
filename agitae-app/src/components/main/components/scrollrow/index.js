4b6d5a3f59e03ad80eb2537092df18a969db508b
Lucas Lima <lucasfsl@al.insper.edu.br>
2023-10-26 20:00:24 -0300

    limite scroll esquerda

--- agitae-app/src/components/main/components/scrollrow/index.js
+++ agitae-app/src/components/main/components/scrollrow/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import './scrollrow.css'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
@@ -6,12 +6,49 @@ import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 export default function ScrollRow(props) {
     const [scrollX, setScrollX] = useState(0);
 
+    const [widthItem, setItemWidth] = useState(null);
+    const [nItens, setNItens] = useState(null);
+    const [widthScrollViewArea, setWidthScrollViewArea] = useState(null);
+
+    useEffect(() => {
+        const [widthitem, nitems, widthscrollviewArea] = calcSizeItemList();
+        setItemWidth(widthitem);
+        setNItens(nitems);
+        setWidthScrollViewArea(widthscrollviewArea);
+    }, []);
+
+    const calcSizeItemList = () => {
+        const item = document.getElementById("teste");
+        const itensList = document.getElementById("item--list");
+        const scrollRowListArea = document.getElementById("scrollRow--listarea");
+        if (item && itensList) {
+            const widthItem = item.offsetWidth;
+            const qntdItens = itensList.childElementCount;
+            const widthScrollRowViewArea = scrollRowListArea.offsetWidth;
+            return [widthItem, qntdItens, widthScrollRowViewArea];
+        }
+        return [null, null, null];
+    };
+
+
+    console.log('');
+
     const handleLeftArrow = () => {
-        
+        if (scrollX + widthItem > 0) {
+            setScrollX(scrollX);
+        } else (
+            setScrollX(scrollX + widthItem)
+        );
+        console.log('Margem: ' + scrollX + ' Largura total: ' + (nItens * widthItem));
     };
 
     const handleRightArrow = () => {
-       
+        if (Math.abs(scrollX + widthItem) > ((nItens * widthItem) - widthScrollViewArea)) {
+            setScrollX(scrollX);
+        } else (
+            setScrollX(scrollX - widthItem)
+        );
+        console.log('Margem: ' + Math.abs(scrollX+widthItem) + ' Largura:' + ((nItens * widthItem) - widthScrollViewArea));
     };
 
     return (
@@ -19,15 +56,16 @@ export default function ScrollRow(props) {
             <div className="scrollRow-left" onClick={handleLeftArrow}>
                 <KeyboardArrowLeftIcon className="iconeScrollLeft" style={{ fontSize: 50 }}></KeyboardArrowLeftIcon>
             </div>
-            <div className="scrollRow--listarea">
-                <div className="scrollRow--list" style={{marginLeft:scrollX}}>
-                    <div className="item--list">
-                        <div className="item"><div className="teste"></div></div>
-                        <div className="item"><div className="teste"></div></div>
-                        <div className="item"><div className="teste"></div></div>
-                        <div className="item"><div className="teste"></div></div>
-                        <div className="item"><div className="teste"></div></div>
-                        <div className="item"><div className="teste"></div></div>
+            <div className="scrollRow--listarea" id="scrollRow--listarea">
+                <div className="scrollRow--list" style={{ marginLeft: scrollX }}>
+                    <div className="item--list" id="item--list">
+                        <div className="item" id="teste"><div className="teste">1</div></div>
+                        <div className="item"><div className="teste">2</div></div>
+                        <div className="item"><div className="teste">3</div></div>
+                        <div className="item"><div className="teste">4</div></div>
+                        <div className="item"><div className="teste">5</div></div>
+                        <div className="item"><div className="teste">6</div></div>
+                        <div className="item"><div className="teste">7</div></div>
                     </div>
                 </div>
             </div>
